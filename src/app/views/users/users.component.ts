35b90154fb1afe278397c73a034455e443627c1b
Ulrich POKA <yorich.poka@gmail.com>
2019-08-21 08:57:06 +0200

    Update application.

--- src/app/views/users/users.component.ts
+++ src/app/views/users/users.component.ts
@@ -5,62 +5,74 @@ import ArrayStore from 'devextreme/data/array_store'
 import { DxDataGridComponent } from 'devextreme-angular';
 import { Role } from 'src/app/models/role.model';
 import { RolesService } from 'src/app/services/roles.service';
-import { EComponent } from 'src/app/enums/component.enum';
-import { BaseViewComponent } from 'src/app/helpers/base.viewcomponent.helper';
 import { UsersService } from 'src/app/services/users.service';
 import { BsModalService } from 'ngx-bootstrap/modal';
 import { FormBuilder, Validators } from '@angular/forms';
 import { FormModal } from 'src/app/helpers/formmodal.helper';
 import { confirm } from 'devextreme/ui/dialog';
 import * as Utils from 'src/app/helpers/utils.helper';
+import { LoadingButton } from 'src/app/helpers/loadingButton.helper';
+import { EFaIcon } from 'src/app/enums/faicon.enum';
 
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
   styleUrls: ['./users.component.css']
 })
-export class UsersComponent extends BaseViewComponent implements OnInit {
+export class UsersComponent implements OnInit {
 
-  formModal       : FormModal;
-  @ViewChild(DxDataGridComponent, { static: false }) dataGrid: DxDataGridComponent;
-  usersDataSource : ArrayStore;
-  rolesDataSource : Role[];
-  selectedItemKeys: any[] = [];
+  formModal                                                   : FormModal;
+  objAddOrUpdated                                             : User = new User();
+  loadingButton                                               : LoadingButton = new LoadingButton('Save', EFaIcon.Check, 'Saving');
+  @ViewChild(DxDataGridComponent, { static: false }) dataGrid : DxDataGridComponent;
+  usersDataSource                                             : ArrayStore;
+  rolesDataSource                                             : Role[];
+  selectedItemKeys                                            : any[] = [];
 
   constructor(
-    private usersService : UsersService,
-    private rolesService : RolesService,
+    private usersService: UsersService,
+    private rolesService: RolesService,
     private modalService: BsModalService,
-    private formBuilder: FormBuilder) {
-      super(EComponent.UsersComponent);
-  }
+    private formBuilder: FormBuilder) { }
 
   ngOnInit() {
-    this.loadUsersDataSource();
-    this.loadRolesDataSource();
-  }
-
-  loadUsersDataSource() : void {
-    this.usersService
-        .readAll()
-        .then(
-          (dataUsers : User[]) => {
-            this.usersDataSource = new ArrayStore({
-              key: "id",
-              data: dataUsers
-            });
+    this.loadUsersDataSource()
+        .finally(
+          () => {
+            this.loadRolesDataSource();
           }
         );
   }
 
-  loadRolesDataSource() : void {
+  loadUsersDataSource(): Promise<void> {
+    return new Promise<void>(
+      (resolve, reject) => {
+        this.usersService
+          .readAll()
+          .then(
+            (dataUsers: User[]) => {
+              this.usersDataSource = new ArrayStore({
+                key: "id",
+                data: dataUsers
+              });
+              resolve();
+            },
+            (reason : any) => {
+              reject(reason);
+            }
+          );
+      }
+    );
+  }
+
+  loadRolesDataSource(): void {
     this.rolesService
-        .readAll()
-        .then(
-          (data : Role[]) => {
-            this.rolesDataSource = data;
-          }
-        );
+      .readAll()
+      .then(
+        (data: Role[]) => {
+          this.rolesDataSource = data;
+        }
+      );
   }
 
   selectionChanged(data: any) {
@@ -68,12 +80,12 @@ export class UsersComponent extends BaseViewComponent implements OnInit {
   }
 
   deleteRecords() {
-      confirm(Utils.messageConfirmdDelete(), 'Confirmation')
+    Utils.confirmdAction()
       .then(
-        (dataBoolean : boolean) => {
-          if(dataBoolean) {
+        (isTrue: boolean) => {
+          if (isTrue) {
             this.selectedItemKeys.forEach((key) => {
-                this.usersDataSource.remove(key);
+              this.usersDataSource.remove(key);
             });
             this.dataGrid.instance.refresh();
           }
@@ -81,58 +93,96 @@ export class UsersComponent extends BaseViewComponent implements OnInit {
       );
   }
 
-  newRecord(template: TemplateRef<any>) : void {
-      this.initDataForm(new User());
-      this.formModal.show(template);
+  newRecord(template: TemplateRef<any>): void {
+    this.objAddOrUpdated = new User();
+    this.initDataForm();
+    this.formModal.show(template);
   }
 
-  submitForm() : void {
+  submitForm(): void {
+    Utils.confirmdAction()
+          .then(
+            (isTrue : boolean) => {
+              if(isTrue) {
+                this.loadingButton.start();
+                if(this.objAddOrUpdated.id === 0) {
+                  this.usersService
+                      .create(this.objAddOrUpdated)
+                      .then(
+                        (dataUser : User) => {
+                          return null;
+                        },
+                        (reason : HttpErrorResponse) => {
+                          return reason;
+                        }
+                      )
+                      .then(
+                        (errorResponse : HttpErrorResponse) => {
+                          this.loadingButton.stop();
+                          if(errorResponse == null) {
+                            this.loadUsersDataSource()
+                                .finally(
+                                  () => {
+                                    this.formModal.hide();
+                                  }
+                                );
+                          } else {
 
+                          }
+                        }
+                      )
+                } else {
+
+                }
+              }
+            }
+          );
   }
 
   // Init form values.
-  initDataForm(user : User) {
+  initDataForm() {
     // Init form.
-    this.formModal =  new FormModal(
-                        this.modalService,
-                        this.formBuilder.group({
-                          id: [
-                            user.id
-                          ],
-                          idRole: [
-                            user.idRole, 
-                            [Validators.required]
-                          ],
-                          login: [
-                            user.login, 
-                            [
-                              Validators.required, 
-                              Validators.minLength(3)
-                            ]
-                          ],
-                          password: [
-                            user.password, 
-                            [
-                              Validators.required, 
-                              Validators.minLength(3)
-                            ]
-                          ],
-                          confirmPassword: [
-                            user.password, 
-                            [
-                              Validators.required, 
-                              Validators.minLength(3)
-                            ]
-                          ],
-                          name: [
-                            user.name, 
-                            [
-                              Validators.required, 
-                              Validators.minLength(3)
-                            ]
-                          ]
-                        })
-                      );
+    this.formModal = new FormModal(
+      this.modalService,
+      this.formBuilder.group({
+        id: [
+          this.objAddOrUpdated.id
+        ],
+        idRole: [
+          this.objAddOrUpdated.idRole,
+          [Validators.required]
+        ],
+        login: [
+          this.objAddOrUpdated.login,
+          [
+            Validators.required,
+            Validators.minLength(3)
+          ]
+        ],
+        password: [
+          this.objAddOrUpdated.password,
+          [
+            Validators.required,
+            Validators.minLength(3)
+          ]
+        ],
+        confirmPassword: [
+          this.objAddOrUpdated.password,
+          [
+            Validators.required,
+            Validators.minLength(3),
+            Utils.validatorMatchValue('password')
+          ]
+        ],
+        name: [
+          this.objAddOrUpdated.name,
+          [
+            Validators.required,
+            Validators.minLength(3)
+          ]
+        ]
+      })
+    );
   }
 
 }
